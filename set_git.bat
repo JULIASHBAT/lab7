d70289fed737b2d395b14d9be851663f3388619b
JULIASHBAT <128640796+JULIASHBAT@users.noreply.github.com>
2024-03-03 21:04:17 +0200

    new

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/JULIASHBAT/EX_7.git
+git remote add origin https://github.com/JULIASHBAT/lab7.git
 git push -u origin main
